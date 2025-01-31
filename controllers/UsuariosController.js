07f2c032bb907a9578be61a3e21fbcbb42f59fb5
GuinhoFSilva <guilherme.silva@sptech.school>
2025-01-31 19:01:26 -0300

    Realiza métodos CRUD completos na tabela usuários

--- controllers/UsuariosController.js
+++ controllers/UsuariosController.js
@@ -33,6 +33,26 @@ class UsuariosController{
             return res.status(500).json(error.message);
         }
     }
+    static async atualizarUsuario(req, res){
+        const {id} = req.params;
+        const novosDados = req.body;
+        try{
+            await database.usuarios.update(novosDados, {where: {id: Number(id)}});
+            const select = await database.usuarios.findOne({where: {id: Number(id)}});
+            return res.status(200).json(select);
+        } catch (error){
+            return res.status(500).json(error.message);
+        }
+    }
+    static async deletarUsuario(req, res){
+        const {id} = req.params;
+        try{
+            await database.usuarios.destroy({where: {id: Number(id)}});
+            return res.status(200).send("Usuário deletado com sucesso!");
+        } catch(error) {
+            return res.status(500).json(error.message);
+        }
+    }
 
 }
 
--- routes/usuarios-route.js
+++ routes/usuarios-route.js
@@ -4,7 +4,9 @@ const UsuariosController = require("../controllers/UsuariosController");
 const router = Router();
 
 router.get('/usuarios', UsuariosController.selecionarTodosUsuarios);
-router.post('/usuarios', UsuariosController.criarUsuario);
 router.get('/usuarios/:id', UsuariosController.selecionarUsuariosPorId);
+router.post('/usuarios', UsuariosController.criarUsuario);
+router.put('/usuarios/:id', UsuariosController.atualizarUsuario);
+router.delete('/usuarios/:id', UsuariosController.deletarUsuario);
 
 module.exports = router;
